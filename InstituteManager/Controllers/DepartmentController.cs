d7f6f7ea28c53c366932cb8bcbefa98af31bb060
Mateus Fortunato <mateusfortunato00@gmail.com>
2021-02-06 22:58:53 -0300

    Partially the api is done, very partially

--- InstituteManager/Controllers/DepartmentController.cs
+++ InstituteManager/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
 using InstituteManager.Data;
+using InstituteManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -19,6 +20,51 @@ namespace InstituteManager.Controllers
         {
             return View(await _context.Departments.OrderBy(o =>
             o.Name).ToListAsync());
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Department department)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _context.Add(department);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(DbUpdateException)
+            {
+                ModelState.AddModelError("", "Was not possible insert the data");
+            }
+
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public async Task<IActionResult> Edit (long? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments.SingleOrDefaultAsync(s => s.DepartmentID == id);
+
+            if(department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
         }
     }
 }
